1b13c609b110ea7b1e54cd5b1c7325c9cd210c7c
cr8Rolo <37343902+cr8Rolo@users.noreply.github.com>
2018-07-10 11:10:21 -0700

    adding exercise_4-11

---
+++ exercise_4-11.c
@@ -0,0 +1,132 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+#define MAXVAL 100
+#define BUFSIZE 100
+
+int sp = 0;
+double val[MAXVAL];
+char buf[BUFSIZE];
+int bufp;
+
+int getop(char []);
+int getch(void);
+void push(double);
+double pop(void);
+
+int main()
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while((type = getop(s)) != EOF)
+    {
+        switch(type)
+        {
+            case NUMBER:
+                    push(atof(s));
+                    break;
+            case '+':
+                    push(pop()+pop());
+                    break;
+            case '*':
+                    push(pop()*pop());
+                    break;
+            case '-':
+                    op2=pop();
+                    push(pop()-op2);
+                    break;
+            case '/':
+                    op2=pop();
+                    if(op2 != 0.0)
+                        push(pop()/op2);
+                    break;
+            case '\n':
+                    printf("\t%.8g\n",pop());
+                    break;
+            default:
+                    printf("error: unknown command %s\n",s);
+                    break;
+        }
+    }
+    return 0;
+}
+
+
+void push(double f)
+{
+  if(sp<MAXVAL)
+    val[sp++] = f;
+  else
+    printf("error: stack full, can't push %g\n",f);
+  
+}
+
+double pop(void)
+{
+    if(sp > 0)
+        return val[--sp];
+    else
+    {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+
+int getop(char s[])
+{
+   static int lastc = 0;
+   int c,i=0;
+ 
+    if(lastc == 0)
+    {
+        c = getch();
+    }
+    else
+    {
+        c = lastc;
+        lastc = 0;
+    }
+
+    while((s[0]=c) == ' ' || c == '\t')
+    {
+        c = getch();
+    }
+  
+    s[1]='\0';
+    
+    if(!isdigit(c) && c!= '.')
+    {
+        return c;
+    }
+    
+    if(isdigit(c))
+    {
+        while(isdigit(s[++i] =c=getch()));
+      
+    }       
+    if(c=='.')
+    {
+      while(isdigit(s[++i] =c=getch()));
+    }
+  
+    s[i]='\0';
+    
+    if(c!=EOF)
+    {
+      lastc=c;
+    }
+
+    return NUMBER;
+}
